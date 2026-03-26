b64f08ddcd3f714734e6a7343c164d4a6b67cce0
Kaleab Bantayehu <kaleabbantayehu@gmail.com>
2026-03-26 17:27:46 +0300

    setup project structure

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import "input_page.dart";
+
+
+void main() {
+  runApp(BMICalculator());
+}
+
+class BMICalculator extends StatelessWidget {
+  const BMICalculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        appBarTheme: AppBarTheme(
+          centerTitle: true,
+          backgroundColor: Color(0xFF0A0E21),
+          titleTextStyle: TextStyle(color: Colors.white, fontSize: 20.0),
+        ),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: const Color.fromARGB(255, 68, 196, 255),
+          foregroundColor: Colors.black,
+        ),
+      ),
+      home: InputPage(),
+    );
+  }
+}
