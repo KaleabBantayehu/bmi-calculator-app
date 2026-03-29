047db48617e350364457486c9bf28c6e64768fbb
Kaleab Bantayehu <kaleabbantayehu@gmail.com>
2026-03-29 22:29:50 +0300

    feat: add getsture detactors to gender selection cards and refactor : integrate reusabe card with gesture detaction

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -3,10 +3,12 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import "icon_content.dart";
 import 'reusable_card.dart';
 
-
 const double bottomContainerHeight = 80.0;
 const activeCardColor = Color(0xFF1D1E33);
+const inactiveCardColor = Color(0xFF111328);
 const buttomContainerColor = Color(0xFFEB1555);
+
+enum Gender { male, female }
 
 class InputPage extends StatefulWidget {
   const InputPage({super.key});
@@ -14,6 +16,8 @@ class InputPage extends StatefulWidget {
   @override
   State<InputPage> createState() => _InputPageState();
 }
+
+Gender? selectedGender;
 
 class _InputPageState extends State<InputPage> {
   @override
@@ -27,16 +31,28 @@ class _InputPageState extends State<InputPage> {
               children: [
                 Expanded(
                   child: ReusableCard(
-                    color: activeCardColor,
-                    cardChild: IconContent(
-                      icon: FontAwesomeIcons.mars,
-                      label: 'MALE',
+                      onPress: () {
+                      setState(() {
+                        selectedGender = Gender.male;
+                      });
+                      },
+                      color: selectedGender == Gender.male
+                          ? activeCardColor
+                          : inactiveCardColor,
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.mars,
+                        label: 'MALE',
+                      ),
                     ),
-                  ),
                 ),
                 Expanded(
                   child: ReusableCard(
-                    color: activeCardColor,
+                    onPress: () {
+                      setState(() {
+                        selectedGender = Gender.female;
+                      });
+                      },
+                    color: selectedGender == Gender.female ? activeCardColor : inactiveCardColor,
                     cardChild: IconContent(
                       icon: FontAwesomeIcons.venus,
                       label: 'FEMALE',
--- lib/reusable_card.dart
+++ lib/reusable_card.dart
@@ -1,20 +1,31 @@
 import "package:flutter/material.dart";
 
+Function onTab = GestureDetector.new;
+
 class ReusableCard extends StatelessWidget {
-  const ReusableCard({super.key, required this.color, this.cardChild});
+  const ReusableCard({
+    super.key,
+    required this.color,
+    this.cardChild,
+    this.onPress,
+  });
 
   final Color color;
   final Widget? cardChild;
+  final GestureTapCallback? onPress;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        color: color,
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: color,
+        ),
+        child: cardChild,
       ),
-      child: cardChild,
     );
   }
 }
